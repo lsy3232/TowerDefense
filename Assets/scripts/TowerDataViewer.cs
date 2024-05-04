309f6e41cf50119cef568031eee6a64c73a202cd
lsy3232 <lhjoyh33@gmail.com>
2024-05-04 14:19:58 +0900

    TowerDefense 6강

--- Assets/scripts/TowerDataViewer.cs
+++ Assets/scripts/TowerDataViewer.cs
@@ -18,6 +18,10 @@ public class TowerDataViewer : MonoBehaviour
     private TextMeshProUGUI textLevel;
     [SerializeField]
     private TowerAttackRange towerAttackRange;
+    [SerializeField]
+    private Button buttonUpgrade;
+    [SerializeField]
+    private SystemTextViewer systemTextViewer;
 
     private TowerWeapon currentTower;
 
@@ -52,9 +56,35 @@ public class TowerDataViewer : MonoBehaviour
 
     private void UpdateTowerData()
     {
+        imageTower.sprite = currentTower.TowerSprite;
         textDamage.text = "Damage : " + currentTower.Damage;
         textRate.text = "Rate : " + currentTower.Rate;
         textRange.text = "Range : " + currentTower.Range;
         textLevel.text = "Level : " + currentTower.Level;
+
+        buttonUpgrade.interactable = currentTower.Level < currentTower.MaxLevel ? true : false;
+    }
+
+    public void OnClickEventTowerUpgrade()
+    {
+        bool isSuccess = currentTower.Upgrade();
+
+        if( isSuccess ==true )
+        {
+            UpdateTowerData();
+
+            towerAttackRange.OnAttackRange(currentTower.transform.position, currentTower.Range);
+        }
+        else
+        {
+            systemTextViewer.PrintText(SystemType.Money);
+        }
+    }
+
+    public void OnClickEventTowerSell()
+    {
+        currentTower.Sell();
+
+        OffPanel();
     }
 }
